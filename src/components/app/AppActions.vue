f2067c9869dc9c0da414b1ac12b0f571120561c7
Brenlys Quintero <brenlysquintero@gmail.com>
2024-12-15 16:26:00 -0400

    tab 4

--- src/components/app/AppActions.vue
+++ src/components/app/AppActions.vue
@@ -47,6 +47,13 @@
               v-model="data.tab3"
             />
           </v-tab-item>
+           <v-tab-item value="tab4">
+            <tab4
+              ref="componentTab4"
+              v-model="data.tab4"
+            />
+          </v-tab-item>
+
         </v-tabs-items>
       </v-card-text>
 
@@ -66,7 +73,7 @@
             </v-btn>
 
             <v-btn
-              v-if="tab !== 'tab3'"
+              v-if="tab !== 'tab4'"
               id="next-button"
               class="px-7 white--text mr-2"
               color="primary"
@@ -98,6 +105,7 @@
 import Tab1 from "@/components/app/tabs/Tab1.vue";
 import Tab2 from "@/components/app/tabs/Tab2.vue";
 import Tab3 from "@/components/app/tabs/Tab3.vue";
+import Tab4 from "./tabs/Tab4.vue";
 import showToast from "@/components/UI/Toast/Toast.vue";
 
 export default {
@@ -107,6 +115,7 @@ export default {
     Tab1,
     Tab2,
     Tab3,
+    Tab4
   },
 
   mixins: [showToast],
@@ -122,11 +131,13 @@ export default {
         { name: "tab1", label: "First" },
         { name: "tab2", label: "Second" },
         { name: "tab3", label: "Third" },
+        { name: "tab4", label: "Fourth" },
       ],
       data: {
         tab1: { name: "", description: "" },
         tab2: { nro1: "", nro2: "" },
         tab3: { note: "" },
+        tab4: { nro1: "", nro2: "" },
       },
     };
   },
@@ -158,15 +169,21 @@ export default {
         }
 
         this.tab = "tab2";
-      } else {
+      } else if(this.tab==='tab2') {
         this.tab = "tab3";
+      }
+        else{
+         this.tab = "tab4";
       }
     },
 
     back() {
-      if (this.tab === "tab3") {
+       if (this.tab === "tab4"){
+          this.tab = "tab3";
+       } 
+      else if (this.tab === "tab3") {
         this.tab = "tab2";
-      } else {
+      } else  {
         this.tab = "tab1";
       }
     },
---
+++ src/components/app/tabs/Tab4.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <v-row class="justify-space-around px-2 mt-1">
+      <v-col cols="5" class="py-0">
+        <v-text-field
+          id="nro1"
+          v-model="data.nro1"
+          label="First Nro"
+          outlined
+          onkeypress="return (event.charCode >= 48 && event.charCode <= 57)"
+        />
+      </v-col>
+
+      <v-col cols="5" class="py-0">
+        <v-text-field
+          id="nro2"
+          v-model="data.nro2"
+          label="Second Nro"
+          outlined
+          onkeypress="return (event.charCode >= 48 && event.charCode <= 57)"
+        />
+      </v-col>
+
+      <v-col cols="2" class="py-0">
+        <v-text-field
+          id="result"
+          v-model="result"
+          label="Result"
+          outlined
+          readonly
+        />
+      </v-col>
+    </v-row>
+
+    <div class="px-2 mt-1 text-center">
+      <v-btn
+        id="sum-button"
+        class="px-7 white--text mr-2"
+        color="primary"
+        rounded
+        depressed
+        @click="div()"
+      >
+        Division
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import showToast from "@/components/UI/Toast/Toast.vue";
+
+export default {
+  name: "Tab-2",
+
+  mixins: [showToast],
+
+  props: {
+    value: { type: Object, default: () => ({}) },
+  },
+
+  data() {
+    return {
+      data: { nro1: "", nro2: "" },
+      result: "",
+    };
+  },
+
+  watch: {
+    data: {
+      handler(value) {
+        this.$emit("input", value);
+      },
+      deep: true,
+    },
+
+    "data.nro1": {
+      handler(value) {
+        if (!value || !this.data.nro2) {
+          this.result = "";
+        }
+      },
+      deep: true,
+    },
+
+    "data.nro2": {
+      handler(value) {
+        if (!value || !this.data.nro1) {
+          this.result = "";
+        }
+      },
+      deep: true,
+    },
+  },
+
+  methods: {
+    sum() {
+      if (!this.data.nro1 || !this.data.nro2) {
+        const message = "You must add both numbers";
+        return this.showToast("", message, "warning");
+      }
+
+      this.result = parseFloat(this.data.nro1) + parseFloat(this.data.nro2);
+    },
+  },
+}
+</script>
