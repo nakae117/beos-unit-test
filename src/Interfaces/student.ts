e7452791ffce2d3e89369f063fd0b9debe130649
Brenlys Quintero <brenlysquintero@gmail.com>
2025-01-22 09:16:18 -0400

    feat: add StudentForm interface for student data management

---
+++ src/Interfaces/student.ts
@@ -0,0 +1,11 @@
+export interface StudentForm {
+  name: string;
+  email: string;
+  identity: string;
+  phone: string;
+  lastName: string;
+  age: number | null;
+  address: string;
+  grade: string;
+  gender: string;
+}
